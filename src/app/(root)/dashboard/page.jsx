f995726423c3e9360f31ae1d977c5a6002c90a95
Vicku Miranda <vikymiri@hotmail.com>
2024-10-22 16:09:53 -0300

    agrego total de perfiles reportados

--- src/app/(root)/dashboard/page.jsx
+++ src/app/(root)/dashboard/page.jsx
@@ -1,38 +1,101 @@
-export default function Dashboard() {
-    return (
-      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-        <h1 className="text-4xl font-bold mb-8">Adoptr dashboard</h1>
+// export default function Dashboard() {
+//     return (
+//       <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+//         <h1 className="text-4xl font-bold mb-8">Adoptr dashboard</h1>
         
-        <hr className="w-2/3 border-t mb-8" />
+//         <hr className="w-2/3 border-t mb-8" />
         
-        <div className="grid grid-cols-3 gap-8">
-          <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
-            <span className="text-lg">Publicaciones reportadas</span>
-            <span className="text-red-600 text-4xl font-bold">4</span>
-          </div>
+//         <div className="grid grid-cols-3 gap-8">
+//           <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
+//             <span className="text-lg">Publicaciones reportadas</span>
+//             <span className="text-red-600 text-4xl font-bold">4</span>
+//           </div>
 
-          <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
-            <span className="text-lg">Perfiles reportados</span>
-            <span className="text-red-600 text-4xl font-bold">2</span>
-          </div>
-          <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
-            <span className="text-lg">Post reportados</span>
-            <span className="text-red-600 text-4xl font-bold">10</span>
-          </div>
-          <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
-            <span className="text-lg">Publicaciones de adopcion reportadas</span>
-            <span className="text-red-600 text-4xl font-bold">2</span>
-          </div>
-          <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
-            <span className="text-lg">Publicaciones de perdidas reportadas</span>
-            <span className="text-red-600 text-4xl font-bold">1</span>
-          </div>
-          <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
-            <span className="text-lg">Publicaciones de servicios reportadas</span>
-            <span className="text-red-600 text-4xl font-bold">1</span>
-          </div>
+//           <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
+//             <span className="text-lg">Perfiles reportados</span>
+//             <span className="text-red-600 text-4xl font-bold">2</span>
+//           </div>
+//           <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
+//             <span className="text-lg">Post reportados</span>
+//             <span className="text-red-600 text-4xl font-bold">10</span>
+//           </div>
+//           <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
+//             <span className="text-lg">Publicaciones de adopcion reportadas</span>
+//             <span className="text-red-600 text-4xl font-bold">2</span>
+//           </div>
+//           <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
+//             <span className="text-lg">Publicaciones de perdidas reportadas</span>
+//             <span className="text-red-600 text-4xl font-bold">1</span>
+//           </div>
+//           <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
+//             <span className="text-lg">Publicaciones de servicios reportadas</span>
+//             <span className="text-red-600 text-4xl font-bold">1</span>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// TODO: DEJO EL ANTERIOR POR LAS DUDAS, ESTA ATADO CON ALAMBRES EL Q HICE, SEGURO HAY UNA MEJOR FORMA DE HACERLO
+
+
+
+'use client';
+
+import { useEffect, useState } from 'react';
+import { getProfileReportsAction } from '@/actions/report';
+import CustomLoading from '@/app/components/customLoading';
+
+export default function Dashboard() {
+  const [totalReportedProfiles, setTotalReportedProfiles] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchReportedProfiles = async () => {
+      try {
+        const reportData = await getProfileReportsAction();
+        setTotalReportedProfiles(reportData.length);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchReportedProfiles();
+  }, []);
+
+  if (loading) return <CustomLoading />;
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold mb-8">Adoptr dashboard</h1>
+      <hr className="w-2/3 border-t mb-8" />
+      <div className="grid grid-cols-3 gap-8">
+        <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
+          <span className="text-lg">Publicaciones reportadas</span>
+          <span className="text-red-600 text-4xl font-bold">4</span>
+        </div>
+        <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
+          <span className="text-lg">Perfiles reportados</span>
+          <span className="text-red-600 text-4xl font-bold">{totalReportedProfiles}</span>
+        </div>
+        <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
+          <span className="text-lg">Post reportados</span>
+          <span className="text-red-600 text-4xl font-bold">10</span>
+        </div>
+        <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
+          <span className="text-lg">Publicaciones de adopción reportadas</span>
+          <span className="text-red-600 text-4xl font-bold">2</span>
+        </div>
+        <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
+          <span className="text-lg">Publicaciones de pérdidas reportadas</span>
+          <span className="text-red-600 text-4xl font-bold">1</span>
+        </div>
+        <div className="bg-white shadow-md p-6 rounded-lg flex flex-col items-center justify-center">
+          <span className="text-lg">Publicaciones de servicios reportadas</span>
+          <span className="text-red-600 text-4xl font-bold">1</span>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
