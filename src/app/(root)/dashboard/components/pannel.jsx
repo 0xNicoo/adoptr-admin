3369f9b038eb93bbbea3008cd491e22f32c003f3
Nicolas Morales <0nicolasmorales@gmail.com>
2024-11-11 23:45:31 -0300

    fix

--- src/app/(root)/dashboard/components/pannel.jsx
+++ src/app/(root)/dashboard/components/pannel.jsx
@@ -1,3 +1,4 @@
+import getMappedCardTittle from "@/util/mapTableTitles";
 import Card from "./card"
 import { Inter } from "next/font/google";
 
@@ -13,7 +14,7 @@ const Pannel = ({title, cardsMap, dataColor}) => {
             <h2 className={`${inter.className} mb-4 text-secondary-blue text-xl font-medium`}>{title}</h2>
             <div className={`${inter.className} grid grid-cols-3 gap-8 mx-4 w-3/4 mb-8`}>
                 {Array.from(cardsMap.entries()).map(([key, value]) => (
-                    <Card key={key} title={key} data={value} dataColor={dataColor}/>
+                    <Card key={key} title={getMappedCardTittle(key)} data={value} dataColor={dataColor}/>
                 ))}
             </div>
         </>
---
+++ src/util/mapTableTitles.js
@@ -0,0 +1,21 @@
+const getMappedCardTittle = (key) => {
+    const mappings = {
+      publicationsCount: "Publicaciones",
+      profilesCount: "Perfiles",
+      postCount: "Post",
+      adoptionPublicationsCount: "Publicaciones de adopciones",
+      adoptedPetsCount: "Mascotas adoptadas",
+      servicePublicationsCount: "Publicaciones de servicio",
+      lostPublicationsCount: "Publicaciones de perdidas",
+      totalPublicationsReported: "Publicaciones reportadas",
+      adoptionReported: "Reportes adopción",
+      lostReported: "Reportes perdidas",
+      serviceReported: "Reportes servicios",
+      postReported: "Reportes post",
+      profileReported: "Reportes perfil"
+    };
+  
+    return mappings[key] || null;
+}
+
+export default getMappedCardTittle
